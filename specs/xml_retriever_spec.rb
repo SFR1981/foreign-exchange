af604732f8a50cf978cb3084b578d756960ad2d3
Stephen Rooney <s.f.rooney@gmail.com>
2018-12-01 03:24:26 +0000

    comments

--- specs/xml_retriever_spec.rb
+++ specs/xml_retriever_spec.rb
@@ -4,13 +4,11 @@ require_relative("../lib/xml_retriever.rb")
 
 class TestXmlRetriever < MiniTest::Test
 
-  def setup
 
-
-  end
 
   def test_xml_is_updated
     XmlRetriever.xmlGet()
+    #compare time now (without milliseconds) with time file was modified 
     assert_equal(Time.now().to_i, File.mtime("../data/fx.xml").to_i)
   end
 
