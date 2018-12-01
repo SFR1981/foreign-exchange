350c52b142372fc7537d7e210228bdef2c1f279c
Stephen Rooney <s.f.rooney@gmail.com>
2018-12-01 21:18:23 +0000

    self methods added

--- specs/exchange_rate_spec.rb
+++ specs/exchange_rate_spec.rb
@@ -4,15 +4,17 @@ require_relative("../lib/exchange_rate.rb")
 
 class TestExchangeRate < MiniTest::Test
 
-  def setup
-    exchange_rate = ExchangeRate.new
-  end
+  # def setup
+  #   exchange_rate = ExchangeRate.new
+  # end
 
+  def test_can_read_file
+    ExchangeRate.at("","","")
+  end
   # ExchangeRate.at can accept date as a string (e.g. from a post from an html form drop down list option)
-  def test_method_returns_exchange_rate__given_date_as_string
-    puts "hi"
-
-  end
+  # def test_method_returns_exchange_rate__given_date_as_string
+  #   p ExchangeRate.at("","","")
+  # end
 
 
   # def test_method_returns_exchange_rate__given_date_as_date_object
