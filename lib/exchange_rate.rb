ae82e58de850e76d16657c179074a0331aa56c2c
Stephen Rooney <s.f.rooney@gmail.com>
2018-12-02 02:16:44 +0000

    added optimise method

--- lib/exchange_rate.rb
+++ lib/exchange_rate.rb
@@ -32,46 +32,49 @@ class ExchangeRate
     return nodeset.empty?
   end
 
-  def self.get_currency_reference_value(date, currency)
+  def self.get_currency_reference_value(date, currency, doc=@doc)
   # querying the xml with nokogiri returns the value as a Nokogiri::XML::NodeSet so the .text operator is used to
   # return a string which is then made into a float for the calculation of the fx rate
-  reference_rate = @doc.xpath('//*[@time="'+"#{date}"+'"]/*[@currency="'+"#{currency}"+'"]/@rate').text.to_f
+  reference_rate = doc.xpath('//*[@time="'+"#{date}"+'"]/*[@currency="'+"#{currency}"+'"]/@rate').text.to_f
   return reference_rate
   end
 
 
-
-
-
-
-
-  def self.validated?(date, base_currency, counter_currency)
-    if date_is_not_found?(date)
+  def self.validated?(date, base_currency, counter_currency, doc=@doc)
+    list_of_currencies = list_currencies_for_date(date, doc)
+    if date_is_not_found?(date, doc)
       return "Date not found. Check that the date is in YYYY-DD-MM format and within the last 90 days"
-    elsif list_currencies_for_date(date).include?(base_currency) == false
-      return "Base currency #{base_currency} not found. Check currency is one of the following #{list_currencies_for_date(date).join(", ")}"
-    elsif list_currencies_for_date(date).include?(counter_currency) == false
-      return "Counter currency #{counter_currency} not found. Check currency is one of the following #{list_currencies_for_date(date).join(", ")}"
+    elsif list_of_currencies.include?(base_currency) == false
+      return "Base currency '#{base_currency}' not found. Check currency is valid"
+    elsif list_of_currencies.include?(counter_currency) == false
+      return "Counter currency '#{counter_currency}' not found. Check currency is valid"
     else
       return true
     end
   end
 
+#purpose of this method is to take a date object and return it as a string
+# it will convert the date to the previous friday if that date falls on a saturday or sunday
+# as it seems fx rates do not update at weekends
+  def self.optimise_date_object(date)
+    if (date.wday == 0 || date.wday== 6)
+      days_before = (date.wday + 1) % 7 + 1
+      return (date.to_date - days_before).to_s
+    else
+      return date.to_s
+    end
+  end
+
+
   def self.at(date, base_currency, counter_currency)
     if File.exist?('../data/fx.xml') == false
       return "the reference file has not been created. Check config/schedule.rb or run cron_job.rb"
-    else
-      if File.zero?('../data/fx.xml')
-        return "the reference file is empty. Check config/schedule.rb or run cron_job.rb"
-      end
+    elsif File.zero?('../data/fx.xml')
+      return "the reference file is empty. Check config/schedule.rb or run cron_job.rb"
+    end
       read_file()
       if date.class == Date
-        previous_date = date - 1
-        previous_date.to_s
-        date.to_s
-        if date_is_not_found?(date)
-          date = previous_date #try previous date as Date.today is sometimes not published within the day
-        end
+        date = optimise_date_object(date)
       end
       if validated?(date, base_currency, counter_currency) == true
         base_currency_value = get_currency_reference_value(date, base_currency)
@@ -81,17 +84,7 @@ class ExchangeRate
       else
         return validated?(date, base_currency, counter_currency)
       end
-
-    end
   end
-
-
-
-
-
-
-
-
 
 
 
